c4e1dfc72639d6a09e4eac0dc7409a59dbbb3010
崮生 <admin@shenzilong.cn>
2025-04-09 01:44:37 +0000

    Update dist to GitHub

--- univer.ts
+++ univer.ts
@@ -1,54 +1,66 @@
 // 引入这个变量后 vite 会自动注入 hot
-import "@univerjs/design/lib/index.css";
-import "@univerjs/ui/lib/index.css";
-import "@univerjs/docs-ui/lib/index.css";
-import "@univerjs/sheets-ui/lib/index.css";
-import "@univerjs/sheets-formula/lib/index.css";
-import "@univerjs/sheets-numfmt/lib/index.css";
-import "@univerjs/sheets-filter-ui/lib/index.css";
+import '@univerjs/design/lib/index.css';
+import '@univerjs/docs-ui/lib/index.css';
+import '@univerjs/sheets-filter-ui/lib/index.css';
+import '@univerjs/sheets-ui/lib/index.css';
+import '@univerjs/ui/lib/index.css';
 
-import { CommandType, LocaleType, Tools, Univer, UniverInstanceType } from "@univerjs/core";
-import { defaultTheme } from "@univerjs/design";
+import { CommandType, LocaleType, merge, Univer, UniverInstanceType } from '@univerjs/core';
+import { defaultTheme } from '@univerjs/design';
 
-import { UniverFormulaEnginePlugin } from "@univerjs/engine-formula";
-import { DeviceInputEventType, UniverRenderEnginePlugin } from "@univerjs/engine-render";
+import { UniverFormulaEnginePlugin } from '@univerjs/engine-formula';
+import { UniverRenderEnginePlugin } from '@univerjs/engine-render';
 
-import { UniverUIPlugin } from "@univerjs/ui";
+import { UniverUIPlugin } from '@univerjs/ui';
 
-import { UniverDocsPlugin } from "@univerjs/docs";
-import { UniverDocsUIPlugin } from "@univerjs/docs-ui";
+import { UniverDocsPlugin } from '@univerjs/docs';
+import { UniverDocsUIPlugin } from '@univerjs/docs-ui';
 
-import { UniverSheetsFilterPlugin } from "@univerjs/sheets-filter";
-import { UniverSheetsFilterUIPlugin } from "@univerjs/sheets-filter-ui";
+import { UniverSheetsFilterPlugin } from '@univerjs/sheets-filter';
+import { UniverSheetsFilterUIPlugin } from '@univerjs/sheets-filter-ui';
 
-import { UniverSheetsPlugin } from "@univerjs/sheets";
-import { UniverSheetsFormulaPlugin } from "@univerjs/sheets-formula";
-import { UniverSheetsUIPlugin } from "@univerjs/sheets-ui";
-import { UniverSheetsNumfmtPlugin } from "@univerjs/sheets-numfmt";
+import { UniverSheetsPlugin } from '@univerjs/sheets';
+import { UniverSheetsFormulaPlugin, CalculationMode } from '@univerjs/sheets-formula';
+import { UniverSheetsNumfmtPlugin } from '@univerjs/sheets-numfmt';
+import { UniverSheetsUIPlugin } from '@univerjs/sheets-ui';
 
-import DesignZhCN from "@univerjs/design/locale/zh-CN";
-import UIZhCN from "@univerjs/ui/locale/zh-CN";
-import DocsUIZhCN from "@univerjs/docs-ui/locale/zh-CN";
-import SheetsZhCN from "@univerjs/sheets/locale/zh-CN";
-import SheetsUIZhCN from "@univerjs/sheets-ui/locale/zh-CN";
-import SheetsFormulaZhCN from "@univerjs/sheets-formula/locale/zh-CN";
-import SheetsNumfmtZhCN from "@univerjs/sheets-numfmt/locale/zh-CN";
-import SheetsFilterUIZhCN from "@univerjs/sheets-filter-ui/locale/zh-CN";
+//#region 排序功能
+import { UniverSheetsSortPlugin } from '@univerjs/sheets-sort';
+import { UniverSheetsSortUIPlugin } from '@univerjs/sheets-sort-ui';
+import '@univerjs/sheets-sort-ui/lib/index.css';
+import SheetsSortUIZhCN from '@univerjs/sheets-sort-ui/locale/zh-CN';
+import '@univerjs/sheets-sort/facade';
+//#endregion 排序功能
+
+import DesignZhCN from '@univerjs/design/locale/zh-CN';
+import DocsUIZhCN from '@univerjs/docs-ui/locale/zh-CN';
+import SheetsFilterUIZhCN from '@univerjs/sheets-filter-ui/locale/zh-CN';
+import SheetsFormulaZhCN from '@univerjs/sheets-formula/locale/zh-CN';
+
+import SheetsUIZhCN from '@univerjs/sheets-ui/locale/zh-CN';
+import SheetsZhCN from '@univerjs/sheets/locale/zh-CN';
+import UIZhCN from '@univerjs/ui/locale/zh-CN';
+import { apiProxy, sendMsg } from '~/libs/apiProxy';
+import { FUNCTION_LIST_USER, functionUser, functionZhCN } from './custom-function';
+import type { checkId } from './msg';
+import pkg from './plugin.json';
+import { FUniver } from '@univerjs/core/facade';
 const univer = new Univer({
   theme: defaultTheme,
   locale: LocaleType.ZH_CN,
   locales: {
-    [LocaleType.ZH_CN]: Tools.deepMerge(
+    [LocaleType.ZH_CN]: merge(
       SheetsZhCN,
       DocsUIZhCN,
       SheetsUIZhCN,
       SheetsFormulaZhCN,
-      SheetsNumfmtZhCN,
+      // SheetsNumfmtZhCN,
       DesignZhCN,
       UIZhCN,
       DesignZhCN,
       functionZhCN,
       SheetsFilterUIZhCN,
+      SheetsSortUIZhCN,
     ),
   },
 });
@@ -59,7 +71,7 @@ univer.registerPlugin(UniverFormulaEnginePlugin, {
 });
 
 univer.registerPlugin(UniverUIPlugin, {
-  container: "univer-siyuan-plugin--app",
+  container: 'univer-siyuan-plugin--app',
 });
 
 univer.registerPlugin(UniverDocsPlugin, {
@@ -71,27 +83,26 @@ univer.registerPlugin(UniverSheetsPlugin);
 univer.registerPlugin(UniverSheetsUIPlugin);
 univer.registerPlugin(UniverSheetsNumfmtPlugin);
 univer.registerPlugin(UniverSheetsFormulaPlugin, {
-  description: FUNCTION_LIST_USER,
+  notExecuteFormula: false,
+  description: [],
+  function: [],
+  initialFormulaComputing: CalculationMode.WHEN_EMPTY,
 });
 univer.registerPlugin(UniverSheetsFilterPlugin);
 univer.registerPlugin(UniverSheetsFilterUIPlugin);
-
-import { FUniver } from "@univerjs/facade";
-import { apiProxy, sendMsg } from "~/libs/apiProxy";
-import pkg from "./plugin.json";
-import type { checkId } from "./msg";
-import { FUNCTION_LIST_USER, functionUser, functionZhCN } from "./custom-function";
+univer.registerPlugin(UniverSheetsSortPlugin);
+univer.registerPlugin(UniverSheetsSortUIPlugin);
 const univerAPI = FUniver.newAPI(univer);
 
 const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get("id")!;
-const copy = urlParams.get("copy");
+const id = urlParams.get('id')!;
+const copy = urlParams.get('copy');
 const api = apiProxy(id);
 
 async function main() {
   const dataPath = `/data/storage/petal/${pkg.name}/univer-${id}.json`;
   const checkPar: checkId = {
-    type: "llej-plugin-rpc-univer-check-id",
+    type: 'llej-plugin-rpc-univer-check-id',
     blockId: id,
   };
 
@@ -108,7 +119,7 @@ async function main() {
   const unit = univer.createUnit(UniverInstanceType.UNIVER_SHEET, data);
   console.log(
     '[document.querySelector(".univer-toolbar-container")?.children]',
-    document.querySelector(".univer-toolbar-container")?.children,
+    document.querySelector('.univer-toolbar-container')?.children,
   );
 
   const save = throttle(async () => {
@@ -116,7 +127,7 @@ async function main() {
     const res = await api.putFile(
       dataPath,
       false,
-      new Blob([JSON.stringify(snapshot)], { type: "text/plain" }),
+      new Blob([JSON.stringify(snapshot)], { type: 'text/plain' }),
     );
   }, 500);
 
